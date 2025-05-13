c84a5c80c53008b3e7f87be12264865bd9302f41
CodyJReed <cody2j2reed@gmail.com>
2025-05-12 23:31:29 -0400

    Add variables, and update on limited types, var, final, and const declarations

--- bin/variables.dart
+++ bin/variables.dart
@@ -1,3 +1,23 @@
 void main() {
-  
+  int num1 = 2;
+  double num2 = 3.0;
+  bool isTrue = true;
+
+  // "is" is the instance-of operator
+  print((num1 + num2) is int);
+  // print runtimetype of an object
+  print((num1 + num2).runtimeType);
+
+  String str = 'hello';
+
+  // String interpolation
+  print('The type of $str is a String ${str is String}');
+
+  var username = '';
+
+  username = "Scarlett";
+
+  final String fullname = "Wiggler";
+
+  const int age = 75;
 }
