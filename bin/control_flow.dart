e414bfdd5285267880e4e79057d1e993b8eb912f
CodyJReed <cody2j2reed@gmail.com>
2025-05-14 22:49:15 -0400

    Add more examples

---
+++ bin/control_flow.dart
@@ -0,0 +1,31 @@
+void main() {
+  
+  String color = 'blue';
+
+  if (color == 'blue') {
+
+  } else if() {
+
+  } else {
+
+  }
+
+  if (color == 'red') print('hello red');
+
+  String thing1 = '';
+
+  if (thing1.isEmpty);
+
+  String? thing2;
+
+  if (thing2 != null);
+
+  for (var i = 0; i <5; i++) {
+    print(i);
+    // break;
+    // continue;
+  }
+
+  var txt = 'good';
+  assert(txt != 'bad');
+}
