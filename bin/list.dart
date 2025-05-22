5f5e81557048ae5952e4f4b26f8cb069e5e6d07e
CodyJReed <cody2j2reed@gmail.com>
2025-05-21 22:29:57 -0400

    add list work

---
+++ bin/list.dart
@@ -0,0 +1,30 @@
+void main() {
+  List<int> list = [1,2,3,4,5];
+
+  list[0];
+
+  list.sublist(2,5);
+
+  var list2 = List.filled(50, 'hello');
+
+  list.add(4);
+  list.removeLast();
+  list.insert(1, 1000);
+
+  for (int n in list) {
+    print(n);
+  }
+
+  list.forEach((n) => print(n));
+
+  var doubled = list.map((n) => n*2);
+
+  var combined = [...list, ...doubled];
+
+  bool depressed = false;
+  var cart = [
+    "milk",
+    "eggs",
+    if (depressed) "Vodka"
+  ];
+}
